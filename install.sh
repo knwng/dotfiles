1f5b95aa363324c7db367ba999c377a0f7982b83
Kyle Wang <ec1wng@gmail.com>
2025-03-05 15:01:55 -0800

    install broot

--- install.sh
+++ install.sh
@@ -4,8 +4,18 @@ cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
 
 if [[ ! -e ~/.zshrc ]]; then
+    echo 'Installing zsh...'
     ./install_zsh.sh
 fi
 
 cat zshrc >> ~/.zshrc
+
+echo 'Installing broot...'
+ROOT=$(pwd)
+mkdir -p ~/.local/bin
+cd ~/.local/bin
+wget https://dystroy.org/broot/download/x86_64-linux/broot
+chmod +x broot
+echo 'export PATH="${HOME}/.local/bin:${PATH}"' >> ~/.zshrc
+cd $ROOT
 
