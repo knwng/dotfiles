bf6dc2c66d47a9e55f99a218e0d267a7def9c3f1
Kyle Wang <ec1wng@gmail.com>
2025-05-15 11:17:05 -0700

    add pid_in_docker function

--- shell_functions.sh
+++ shell_functions.sh
@@ -12,3 +12,9 @@ function findir() {
 }
 export findir
 
+function pid_in_docker() {
+    PID=$1
+    docker ps --no-trunc --format '{{.ID}} {{.Names}}' | grep $(cat /proc/$PID/cgroup | grep 'docker' | sed -E 's|.*docker-([a-f0-9]+)\.scope|\1|' | head -n1) | awk '{print $2}'
+}
+export pid_in_docker
+
