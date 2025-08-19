3c2c3c6c654ac87318c25b2bb6c62f91a44a2c2d
Kyle Wang <ec1wng@gmail.com>
2025-08-19 15:10:19 -0700

    install wget tmux and so on

--- install_zsh.sh
+++ install_zsh.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 sudo apt-get update -y
-sudo apt-get install -y zsh
+sudo apt-get install -y zsh wget tmux vim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
